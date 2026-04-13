c0e6ec419c3d0e3039b4af9f7e47c5a847ccc452
hoodlover <lance.climb@gmail.com>
2026-04-12 23:58:42 -0400

    Auto-save changes

--- friends-sw.js
+++ friends-sw.js
@@ -31,6 +31,7 @@ const PRECACHE_URLS = [
   // emergency icons
   '/webpic/hospicon.png',
   '/webpic/policon.png',
+  '/webpic/wpaicon.jpg',
   // checkout
   '/webpic/bidet.webp',
   '/webpic/$200.webp',
